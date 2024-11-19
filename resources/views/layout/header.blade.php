07819230462ebd803ba2563e7dd9de3e9ddc0955
bilalrao482 <bilal@destring.com>
2024-11-19 11:45:24 +0500

    updated

--- resources/views/layout/header.blade.php
+++ resources/views/layout/header.blade.php
@@ -1,36 +1,39 @@
 <!doctype html>
-<html lang="en" >
-  <head>
+<html lang="en">
+
+<head>
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.rtl.min.css" integrity="sha384-dpuaG1suU0eT09tx5plTaGMLBsfDLzUCCUXOY2j/LSvXYuG6Bqs43ALlhIqAJVRb" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.rtl.min.css"
+        integrity="sha384-dpuaG1suU0eT09tx5plTaGMLBsfDLzUCCUXOY2j/LSvXYuG6Bqs43ALlhIqAJVRb" crossorigin="anonymous">
     <link rel="stylesheet" href="assets/css/style.css">
+
+    {{--  these are the links that are used when we use yajra datatables  --}}
     <link href="https://cdn.datatables.net/1.13.0/css/jquery.dataTables.min.css" rel="stylesheet">
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-<!-- DataTables JS -->
-<script src="https://cdn.datatables.net/1.13.0/js/jquery.dataTables.min.js"></script>
+    <!-- DataTables JS -->
+    <script src="https://cdn.datatables.net/1.13.0/js/jquery.dataTables.min.js"></script>
+    {{--  -------------------------------------------------  --}}
     <title>Crud</title>
-  </head>
-  <body>
+</head>
+
+<body>
 
 
-        <nav class="navbar bg-dark border-bottom border-body" data-bs-theme="dark">
-            <div class="container">
-              <a class="navbar-brand">CRUD Operation</a>
-              <div class="d-flex gap-2">
+    <nav class="navbar bg-dark border-bottom border-body" data-bs-theme="dark">
+        <div class="container">
+            <a class="navbar-brand">CRUD Operation</a>
+            <div class="d-flex gap-2">
                 @if (Auth::check())
-                <h4 class="text-white"> Hi, {{ Auth::user()->name }}</h4>
+                    <h4 class="text-white"> Hi, {{ Auth::user()->name }}</h4>
                 @else
-                <a class="btn btn-primary" href="{{ route('login') }}">Login</a>
-                <a class="btn btn-primary" href="{{ route('register') }}">Register</a>
+                    <a class="btn btn-primary" href="{{ route('login') }}">Login</a>
+                    <a class="btn btn-primary" href="{{ route('register') }}">Register</a>
                 @endif
 
             </div>
-            </div>
-          </nav>
-
-
-
+        </div>
+    </nav>
